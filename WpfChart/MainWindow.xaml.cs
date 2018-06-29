24acd5f5a6b56cc94736bd164e10a6df0e8357a1
Prateek Chaplot <prateek.chaplot@metacube.com>
2018-06-29 17:31:51 +0530

    06-29-2018 5:32

--- WpfChart/MainWindow.xaml.cs
+++ WpfChart/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,47 +13,45 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Collections.ObjectModel;
 
-namespace WpfApp
+namespace WpfChart
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
     public class Country
     {
-        public Country(string Name, long Population)
+        public Country(string Name, long Population)    // Constructor
         {
             this.Name = Name;
             this.Population = Population;
         }
-        public string Name
+        public string Name                // Name Property
         {
             get;
             set;
         }
-        public long Population
+        public long Population                // Population Property
         {
             get;
             set;
         }
     }
-    public class CountryCollection : Collection<Country>
+
+    public class CountryCollection : Collection<Country> // Extending from System.Collections.ObjectModel.Collection class
     {
-        public CountryCollection()
+        public CountryCollection() // Constructor to add Country objects to the CountryCollection
         {
             Add(new Country("China", 1336718015));
             Add(new Country("India", 1189172906));
             Add(new Country("United States", 313232044));
             Add(new Country("Indonesia", 245613043));
             Add(new Country("Brazil", 203429773));
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
         }
     }
 }
---
+++ WpfRealTimeData/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+
+namespace WpfRealTimeData
+{
+    public partial class MainWindow : Window
+    {
+        List<KeyValuePair<int, int>> MyValue = new List<KeyValuePair<int, int>>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            showChart();
+        }
+
+        private void showChart()
+        {
+            Random r = new Random();
+            for(int i = 0; i < 20; i++)
+            {
+                MyValue.Add(new KeyValuePair<int, int>(i, r.Next(25, 51)));
+            }
+
+            lineChart.DataContext = MyValue;
+        }
+    }
+}
